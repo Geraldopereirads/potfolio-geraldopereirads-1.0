bca77c516897ac14e9a350ef0684bf9e5de53913
geraldopereirads <geraldo.pereirads@hotmail.com>
2023-09-22 19:38:36 -0300

    fix: repair responsividade

--- src/pages/ToolsPage/MainToolsPage/MainToolsList/mainTitleListStyle.ts
+++ src/pages/ToolsPage/MainToolsPage/MainToolsList/mainTitleListStyle.ts
@@ -8,14 +8,39 @@ export const MainTitleListStyleOne = styled.div`
   align-items: center;
   flex-direction: row;
 
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 15px;
+  }
+
   h2 {
     font-weight: bold;
-    font-size: 1.5rem;
+    font-size: 1.6rem;
   }
 
   small {
     font-weight: bold;
     font-size: 0.85rem;
+  }
+
+  @media (min-width: 500px) {
+    h2 {
+      font-size: 2rem;
+    }
+    small {
+      font-size: 1.2rem;
+    }
+  }
+
+  @media (min-width: 1000px) {
+    h2 {
+      font-size: 2.7rem;
+    }
+    small {
+      font-size: 1.5rem;
+    }
   }
 `;
 
